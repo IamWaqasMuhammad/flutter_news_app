b96d1ee614fb6e5fd3beb9e06b0176bb99904a65
IamWaqasMuhammad <waqas.developer5254@gmail.com>
2025-11-22 09:51:57 +0500

    app images add

---
+++ lib/constants/app_images/app_images.dart
@@ -0,0 +1,22 @@
+class AppImagesAssets {
+
+  static const String baseUrl = 'assets/images';
+
+  //---------------------------------------------------------------
+  //----------------PNG IMAGES--------------------------------
+  //--------------------------------------------------------
+
+
+
+
+
+  //---------------------------------------------------------------
+  //----------------SVG IMAGES--------------------------------
+  //--------------------------------------------------------
+  /// ONBOARDING IMAGES
+  static const String onboardingImage1 = '$baseUrl/img_onboarding1.svg';
+  static const String onboardingImage2 = '$baseUrl/img_onboarding2.svg';
+  static const String onboardingImage3 = '$baseUrl/img_onboarding3.svg';
+
+
+}
